e90cad7c1ea34282062c1822d03031b824acdd4b
Alteian <Alteian@proton.me>
2023-07-22 19:05:38 +0200

    path vars

--- src/commands/converter.py
+++ src/commands/converter.py
@@ -2,23 +2,19 @@ import nbformat
 
 from nbconvert import PythonExporter
 from src.config.settings import BASE_DIR
-from pathlib import Path
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-def convert_ipynb_to_py(notebook_path, output_path):
+def convert_ipynb_to_py(notebook_path: str, output_path: str) -> None:
     with open(notebook_path) as nb_file:
         nb_contents = nb_file.read()
         notebook = nbformat.reads(nb_contents, as_version=4)
     
     exporter = PythonExporter()
     source_code, _ = exporter.from_notebook_node(notebook)
-    source_code.replace("data/F1_training.csv", str(BASE_DIR / "algo" / "data" / "F1_training.csv"))
-    source_code.replace("model.pickle", str(BASE_DIR / "algo" / "model.pickle"))
+    source_code = source_code.replace("data/F1_training.csv", str(BASE_DIR / "algo" / "data" / "F1_training.csv"))
+    source_code = source_code.replace("model.pickle", str(BASE_DIR / "algo" / "model.pickle"))
     with open(output_path, "w") as py_file:
         py_file.write(source_code)
 
-
-notebook_path = "src/algo/training.ipynb"
-output_path = "src/algo/converted_script.py"
+notebook_path = str(BASE_DIR / "algo" / "training.ipynb")
+output_path = str(BASE_DIR / "algo" / "converted_script.py")
 convert_ipynb_to_py(notebook_path, output_path)
