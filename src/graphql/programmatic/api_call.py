9b2753e4d2057852d64d7e10743c302cdac81b63
Alteian <Alteian@proton.me>
2023-07-24 00:14:44 +0200

    hints added

---
+++ src/graphql/programmatic/api_call.py
@@ -0,0 +1,56 @@
+import httpx
+
+default_input_data = {
+    "Eddie Johnson": {
+        "Race_Entries": 9.0,
+        "Race_Starts": 9.0,
+        "Pole_Positions": 0.0,
+        "Race_Wins": 0.0,
+        "Fastest_Laps": 0.0
+    },
+    "Paul Belmondo": {
+        "Race_Entries": 27.0,
+        "Race_Starts": 7.0,
+        "Pole_Positions": 0.0,
+        "Race_Wins": 0.0,
+        "Fastest_Laps": 0.0
+    },
+    "Tarso Marques": {
+        "Race_Entries": 26.0,
+        "Race_Starts": 24.0,
+        "Pole_Positions": 0.0,
+        "Race_Wins": 0.0,
+        "Fastest_Laps": 0.0
+    },
+    "Tom Bels\u00f8": {
+        "Race_Entries": 5.0,
+        "Race_Starts": 2.0,
+        "Pole_Positions": 0.0,
+        "Race_Wins": 0.0,
+        "Fastest_Laps": 0.0
+    }
+}
+    
+@staticmethod
+def make_api_call(input_data: str = default_input_data, api_endpoint: str = "http://t-mobile-task.eu-west-1.elasticbeanstalk.com/graphql") -> None:
+    """
+    Make a GraphQL API call to the T-Mobile Task API.
+    """
+    query = """
+    query PredictChampions($input: JSON!) {
+        predictChampions(input: $input)
+    }
+    """
+    variables = {
+        "input": input_data
+    }
+    response = httpx.post(api_endpoint, json={"query": query, "variables": variables})
+    if response.status_code == 200:
+        data = response.json()
+        if "errors" in data:
+            print("GraphQL Error:", data["errors"])
+        else:
+            result = data["data"]["predictChampions"]
+            print("Prediction Result:", result)
+    else:
+        print("HTTP Error:", response.status_code, response.text)
--- src/graphql/queries/prediction.py
+++ src/graphql/queries/prediction.py
@@ -1,21 +1,51 @@
 import strawberry
 import typing
+import textwrap
 
 from strawberry.scalars import JSON
 from strawberry.types import Info
 
 from src.utils import model_predict_from_input
+from src.config.settings import BASE_DIR
 
 
 @strawberry.type
 class PredictionQuery:
-    @strawberry.field
+    @strawberry.field(
+    description="""Queries for the prediction of the F1 World Champions.
+    Args --> variables:
+    {
+        "input": {
+            "driver_name": {
+                "Race_Entries": "float",
+                "Race_Starts": "float",
+                "Pole_Positions": "float",
+                "Race_Wins": "float",
+                "Fastest_Laps": "float"
+            },
+            ...
+    }
+        """,
+        deprecation_reason="JSON input is generally not recommended as it cannot be typed. Better use GraphQL Objects with proper typing."
+                 )
     async def predict_champions(
         self,
         info: Info,
         input: JSON) -> JSON:
+        if not input:
+            raise Exception("Input is empty.")
         try:
             ret_val: typing.Dict[str, float] = model_predict_from_input(info.context.model, input)
             return ret_val
         except Exception as e:
             raise Exception(str(e))
+
+    @strawberry.field
+    async def api_call_showcase(
+        self,
+        info: Info,
+        ) -> str:
+        with open(BASE_DIR / "graphql" / "programmatic" / "api_call.py", "r") as f:
+            api_call_source = f.read()
+            raw_string = textwrap.dedent(api_call_source)
+        return raw_string
